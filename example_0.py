4db013d46d3644aa11208a5ad7701ccb4049e00b
Juan Alejandro Pérez Bermúdez <70547458+Mega-Barto@users.noreply.github.com>
2024-09-19 09:28:34 -0500

    feat: documentación de example_0.py

--- example_0.py
+++ example_0.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
+driver = webdriver.Firefox() #Definición del webdriver a utilizar (Firefox, Chrome, etc.)
+driver.get("http://www.python.org") #El get indica cual va a ser la página a levantar
+assert "Python" in driver.title #Assert #1
+elem = driver.find_element(By.NAME, "q") # Localización de elementos del DOM
+elem.clear() # Interacción con el elemento
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+assert "No results found." not in driver.page_source # Assert #2
+driver.close() #Cerrar la pestaña
